d2fca07db04fd833b7a1136dad84b7c668ffa21d
Vitou Phy <henry9669@ymail.com>
2018-01-19 23:14:26 +0700

    modify sieve prime testing

--- test/numbers/sieve_prime.test.js
+++ test/numbers/sieve_prime.test.js
@@ -1,27 +1,21 @@
 var PrimeNumber = require('../../src/numbers/prime_number/PrimeNumber');
 
-test ('test sieve prime of 101', () => {
-    var arr = PrimeNumber.sieve_prime(101);
-
-    expect(arr[1]).toBe(0);
-    expect(arr[2]).toBe(1);
-    expect(arr[57]).toBe(0);
-    expect(arr[60]).toBe(0);
-    expect(arr[91]).toBe(0);
-    expect(arr[100]).toBe(0);
-    expect(arr[101]).toBe(1);
-
+test('test is_prime function', () => {
+    expect(PrimeNumber.is_prime(-1)).toBe(false);
+    expect(PrimeNumber.is_prime(0)).toBe(false);
+    expect(PrimeNumber.is_prime(2)).toBe(true);
+    expect(PrimeNumber.is_prime(1)).toBe(false);
+    expect(PrimeNumber.is_prime(5)).toBe(true);
+    expect(PrimeNumber.is_prime(57)).toBe(false);
+    expect(PrimeNumber.is_prime(101)).toBe(true);
 })
 
-test ('test sieve prime of 100', () => {
-    var arr = PrimeNumber.sieve_prime(100);
+test ('test sieve prime of 101', () => {
+    var arr = PrimeNumber.sieve_prime(101);
+    for(var i=2; i<=101; i++) {
+        expect(arr[i]).toBe(PrimeNumber.is_prime(i));
+    }
 
-    expect(arr[1]).toBe(0);
-    expect(arr[2]).toBe(1);
-    expect(arr[57]).toBe(0);
-    expect(arr[60]).toBe(0);
-    expect(arr[91]).toBe(0);
-    expect(arr[100]).toBe(0);
 })
 
 test ('test sieve prime of n=1', () => {
