0c183251913369d0e9d1bcea526c58dab192a96a
Vitou Phy <henry9669@ymail.com>
2018-01-19 23:02:37 +0700

    add is_prime and testing

---
+++ src/numbers/prime_number/PrimeNumber.js
@@ -0,0 +1,36 @@
+function sieve_prime(n) {
+
+    if (n < 2) {
+        return null;
+    }
+
+    var arr = Array.apply(null, Array(n+1)).map(Boolean.prototype.valueOf, true);
+    arr[0] = false;
+    arr[1] = false;
+
+    var sqrt_n = Math.sqrt(n)
+    for (var i=2; i<=sqrt_n; i++) {
+        if (arr[i]) {
+            for (var j=i*i; j<=n; j+=i) {
+                arr[j] = false;
+            }
+        }
+    }
+
+    return arr
+}
+
+function is_prime(n) {
+    if (n<2) return false;
+    for (var i=2; i<n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = {
+    sieve_prime,
+    is_prime
+};
