75fa66c17b4ce001ebf0bd9e7376a7f2069ce80c
Visal .In <invisal@gmail.com>
2018-04-26 12:21:13 +0700

    Add LCG random algorithm

--- src/numbers/random/LinearCongruentialGenerator.js
+++ src/numbers/random/LinearCongruentialGenerator.js
@@ -6,6 +6,11 @@ class LinearCongruentialGenerator
 
   next() {
     this.seed = (1103515245 * this.seed + 12345) % 2147483648;
+    return this.seed;
+  }
+
+  nextRange(min, max) {
+    return min + next() % (max - min + 1);
   }
 }
 
