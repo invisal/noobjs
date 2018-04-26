42b5cfaae6d9fb6ef28f5e294bbb672a340cb72e
Visal .In <invisal@gmail.com>
2018-04-26 12:22:24 +0700

    Add more algorithm to entry point

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 module.exports = {
-    LinearRegression: require('./src/ml/LinearRegression.js'),
-    KMean: require('./src/ml/KMeanCluster.js'),
-    GaussianNaiveBayes: require('./src/ml/GaussianNaiveBayes.js'),
-    KNeighborsClassifier: require('./src/ml/KNeighborsClassifier.js'),
+  LinearRegression: require('./src/ml/LinearRegression.js'),
+  KMean: require('./src/ml/KMeanCluster.js'),
+  GaussianNaiveBayes: require('./src/ml/GaussianNaiveBayes.js'),
+  KNeighborsClassifier: require('./src/ml/KNeighborsClassifier.js'),
 
-    PriorityQueue: require('./src/collections/PriorityQueue'),
-    HashPriorityQueue: require('./src/collections/HashPriorityQueue'),
-    DisjointSet: require('./src/collections/DisjointSet'),
-    Queue: require('./src/collections/Queue'),
-    Stack: require('./src/collections/Stack'),
+  PriorityQueue: require('./src/collections/PriorityQueue'),
+  HashPriorityQueue: require('./src/collections/HashPriorityQueue'),
+  DisjointSet: require('./src/collections/DisjointSet'),
+  Queue: require('./src/collections/Queue'),
+  Stack: require('./src/collections/Stack'),
 
-    LinearAlgebra: require('./src/numbers/LinearAlgebra'),
-    BigNumber: require('./src/numbers/BigNumber'),
-    RandomNumber: require('./src/numbers/RandomNumber'),
+  LinearAlgebra: require('./src/numbers/LinearAlgebra'),
+  BigNumber: require('./src/numbers/BigNumber'),
+  RandomNumber: require('./src/numbers/RandomNumber'),
+
+  FisherYatesShuffle: require('./src/other/FisherYatesShuffle'),
+  Partition: require('./src/other/Partition'),
 }
